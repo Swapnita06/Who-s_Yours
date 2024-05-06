703c4b1a190eb5d8831ee035589745ed3ff5b354
Swapnita Singh <swapimamuni@gmail.com>
2024-05-06 13:24:24 +0530

    chatmessagechanges

--- frontend/src/components/chats/Chaty.jsx
+++ frontend/src/components/chats/Chaty.jsx
@@ -3,8 +3,13 @@ import './Chaty.css'
 import EmojiPicker from'emoji-picker-react'
 import { useState } from 'react'
 const Chaty = () => {
-const [open,setOpen]= useState(false)
+const [open,setOpen]= useState(false);
+const [text,setText]= useState("");
 
+const handleEmoji =e =>{
+  setText((prev)=>prev+ e.emoji);
+  setOpen(false)
+}
 
   return (
     <div className='chat'>
@@ -21,17 +26,110 @@ const [open,setOpen]= useState(false)
           <img src='./images/info.png'/>
         </div>
       </div>
-      <div className="center"></div>
+      <div className="center">
+        <div className="message own">
+          <div className="texts">
+            <p>Lorem ipsum dolor sit amet consectetur 
+              adipisicing elit. Iusto quo, dolorum quam
+               ullam odit velit neque suscipit eligendi eos 
+               ratione labore? Quidem tenetur officia, 
+              magni est corporis molestiae animi perferendis.
+              </p>
+              <span>1min ago</span>
+          </div>
+        </div>
+
+        <div className="message">
+          <img src='./images/avatar.png'/>
+          <div className="texts">
+            <p>Lorem ipsum dolor sit amet consectetur 
+              adipisicing elit. Iusto quo, dolorum quam
+               ullam odit velit neque suscipit eligendi eos 
+               ratione labore? Quidem tenetur officia, 
+              magni est corporis molestiae animi perferendis.
+              </p>
+              <span>1min ago</span>
+          </div>
+        </div>
+
+        <div className="message own">
+          <div className="texts">
+            <p>Lorem ipsum dolor sit amet consectetur 
+              adipisicing elit. Iusto quo, dolorum quam
+               ullam odit velit neque suscipit eligendi eos 
+               ratione labore? Quidem tenetur officia, 
+              magni est corporis molestiae animi perferendis.
+              </p>
+              <span>1min ago</span>
+          </div>
+        </div>
+
+        <div className="message">
+          <img src='./images/avatar.png'/>
+          <div className="texts">
+            <p>Lorem ipsum dolor sit amet consectetur 
+              adipisicing elit. Iusto quo, dolorum quam
+               ullam odit velit neque suscipit eligendi eos 
+               ratione labore? Quidem tenetur officia, 
+              magni est corporis molestiae animi perferendis.
+              </p>
+              <span>1min ago</span>
+          </div>
+        </div>
+
+        <div className="message own">
+          <div className="texts">
+            <p>Lorem ipsum dolor sit amet consectetur 
+              adipisicing elit. Iusto quo, dolorum quam
+               ullam odit velit neque suscipit eligendi eos 
+               ratione labore? Quidem tenetur officia, 
+              magni est corporis molestiae animi perferendis.
+              </p>
+              <span>1min ago</span>
+          </div>
+        </div>
+
+        <div className="message">
+          <img src='./images/avatar.png'/>
+          <div className="texts">
+            <p>Lorem ipsum dolor sit amet consectetur 
+              adipisicing elit. Iusto quo, dolorum quam
+               ullam odit velit neque suscipit eligendi eos 
+               ratione labore? Quidem tenetur officia, 
+              magni est corporis molestiae animi perferendis.
+              </p>
+              <span>1min ago</span>
+          </div>
+        </div>
+
+        <div className="message own">
+          <div className="texts">
+            <img src="./images/dating_app.gif"/>
+            <p>Lorem ipsum dolor sit amet consectetur 
+              adipisicing elit. Iusto quo, dolorum quam
+               ullam odit velit neque suscipit eligendi eos 
+               ratione labore? Quidem tenetur officia, 
+              magni est corporis molestiae animi perferendis.
+              </p>
+              <span>1min ago</span>
+          </div>
+        </div>
+
+      </div>
       <div className="bottom">
         <div className='icons' >
           <img src='./images/img.png'/>
           <img  src='./images/camera.png'/>
           <img src='./images/mic.png'/>
         </div>
-        <input type='text' placeholder='Type a message...'/>
+        <input type='text' placeholder='Type a message...' 
+        value={text}
+        onChange={e=>setText(e.target.value)}/>
       <div className='emoji'>
         <img src='./images/emoji.png' onClick={()=> setOpen((prev)=>!prev)}/>
-        <EmojiPicker open={open} />
+        <div className="picker">
+        <EmojiPicker open={open}  onEmojiClick={handleEmoji}/>
+        </div>
       </div>
       <button className='sendButton'>Send</button>
       </div>
