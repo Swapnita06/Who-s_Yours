32987a0761ce434d0f481f63253e00dc1b1ae28a
Swapnita Singh <swapimamuni@gmail.com>
2024-05-07 01:03:20 +0530

    changecss

--- frontend/src/components/login/Login.jsx
+++ frontend/src/components/login/Login.jsx
@@ -1,7 +1,22 @@
 import './login.css'
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+const[avatar,setAvatar]= useState({
+    file:null,
+    url:""
+})
+    
+    const handleAvatar = e =>{
+        if(e.target.files[0]){
+        setAvatar({
+            file:e.target.files[0],
+            url:URL.createObjectURL(e.target.files[0])
+
+        })
+    }
+}
+
   return (
     <div className='login'>
       <div className='item'>
@@ -16,11 +31,14 @@ const Login = () => {
       <div className="item">
       <h2>Create an Account</h2>
         <form>
-            <input type='file' id='file'/>
+            <label htmlFor='file'>
+                <img src={avatar.url||"./images/avatar.png"} alt="" />                Upload an Image
+            </label>
+            <input type='file' id='file' style={{display:'none'}} onChange={handleAvatar} />
             <input type='text' placeholder='Username' name='username'/>
             <input type='text' placeholder='Email ' name='email'/>
             <input type='password' placeholder='Password' name='password'/>
-             <button>Sign In</button>
+             <button>Sign Up</button>
         </form>
       </div>
     </div>
